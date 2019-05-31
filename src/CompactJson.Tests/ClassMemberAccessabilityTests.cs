28df876534b4fb47d8d37b135f3a3c606e30694c
pupsette <weigole@gmx.de>
2019-05-31 22:32:50 +0200

    Added tests for private, protected and internal fields.

--- src/CompactJson.Tests/ClassMemberAccessabilityTests.cs
+++ src/CompactJson.Tests/ClassMemberAccessabilityTests.cs
@@ -7,17 +7,16 @@ namespace CompactJson.Tests
     [TestFixture]
     public class ClassMemberAccessabilityTests
     {
-        private class TestClassPrivateMembers
+        private class TestClass
         {
-            public TestClassPrivateMembers()
+            public TestClass()
             {
             }
 
-            public TestClassPrivateMembers(string privateSetterPropertyValue, string privatePropertyValue, string privateFieldValue, string privateReadonlyFieldValue)
+            public TestClass(string privateSetterPropertyValue, string privatePropertyValue, string privateReadonlyFieldValue)
             {
                 PrivateSetter = privateSetterPropertyValue;
                 PrivateProperty = privatePropertyValue;
-                PrivateField = privateFieldValue;
                 PrivateReadOnlyField = privateReadonlyFieldValue;
             }
 
@@ -30,16 +29,113 @@ namespace CompactJson.Tests
 
             private string PrivateProperty { get; set; }
 
-            private string PrivateField;
-
             [JsonProperty]
-            private readonly string PrivateReadOnlyField;
+            public readonly string PrivateReadOnlyField;
         }
 
         [Test]
         public void Parsing_into_readonly_field_should_fail()
         {
-            Assert.That(() => Serializer.Parse<TestClassPrivateMembers>("{ \"PrivateReadOnlyField\": \"123\" }"), Throws.Exception);
+            Assert.That(() => Serializer.Parse<TestClass>("{ \"PrivateReadOnlyField\": \"123\" }"), Throws.Exception);
+        }
+
+        private class PrivateFields
+        {
+            [JsonProperty]
+            private string PrivateFieldWithAttribute = "333";
+
+            private string PrivateFieldWithoutAttribute = "456";
+
+            public string GetPrivateFieldWithoutAttribute() { return PrivateFieldWithoutAttribute; }
+
+            public string GetPrivateFieldWithAttribute() { return PrivateFieldWithAttribute; }
+        }
+
+        [Test]
+        public void Private_field_should_be_ignored_when_not_attributed()
+        {
+            PrivateFields obj = Serializer.Parse<PrivateFields>("{ \"PrivateFieldWithoutAttribute\": \"123\" }");
+            Assert.That(obj.GetPrivateFieldWithoutAttribute(), Is.EqualTo("456"));
+        }
+
+        [Test]
+        public void Private_field_should_be_parsed_when_attributed()
+        {
+            PrivateFields obj = Serializer.Parse<PrivateFields>("{ \"PrivateFieldWithAttribute\": \"123\" }");
+            Assert.That(obj.GetPrivateFieldWithAttribute(), Is.EqualTo("123"));
+        }
+
+        [Test]
+        public void Private_fields_are_emitted_according_to_attributes()
+        {
+            string json = Serializer.ToString(new PrivateFields(), false);
+            Assert.That(json, Is.EqualTo("{\"PrivateFieldWithAttribute\":\"333\"}"));
+        }
+
+        private class ProtectedFields
+        {
+            [JsonProperty]
+            protected string ProtectedFieldWithAttribute = "333";
+
+            protected string ProtectedFieldWithoutAttribute = "456";
+
+            public string GetProtectedFieldWithoutAttribute() { return ProtectedFieldWithoutAttribute; }
+
+            public string GetProtectedFieldWithAttribute() { return ProtectedFieldWithAttribute; }
+        }
+
+        [Test]
+        public void Protected_field_should_be_ignored_when_not_attributed()
+        {
+            ProtectedFields obj = Serializer.Parse<ProtectedFields>("{ \"ProtectedFieldWithoutAttribute\": \"123\" }");
+            Assert.That(obj.GetProtectedFieldWithoutAttribute(), Is.EqualTo("456"));
+        }
+
+        [Test]
+        public void Protected_field_should_be_parsed_when_attributed()
+        {
+            ProtectedFields obj = Serializer.Parse<ProtectedFields>("{ \"ProtectedFieldWithAttribute\": \"123\" }");
+            Assert.That(obj.GetProtectedFieldWithAttribute(), Is.EqualTo("123"));
+        }
+
+        [Test]
+        public void Protected_fields_are_emitted_according_to_attributes()
+        {
+            string json = Serializer.ToString(new ProtectedFields(), false);
+            Assert.That(json, Is.EqualTo("{\"ProtectedFieldWithAttribute\":\"333\"}"));
+        }
+
+        private class InternalFields
+        {
+            [JsonProperty]
+            internal string InternalFieldWithAttribute = "333";
+
+            internal string InternalFieldWithoutAttribute = "456";
+
+            public string GetInternalFieldWithoutAttribute() { return InternalFieldWithoutAttribute; }
+
+            public string GetInternalFieldWithAttribute() { return InternalFieldWithAttribute; }
+        }
+
+        [Test]
+        public void Internal_field_should_be_ignored_when_not_attributed()
+        {
+            InternalFields obj = Serializer.Parse<InternalFields>("{ \"InternalFieldWithoutAttribute\": \"123\" }");
+            Assert.That(obj.GetInternalFieldWithoutAttribute(), Is.EqualTo("456"));
+        }
+
+        [Test]
+        public void Internal_field_should_be_parsed_when_attributed()
+        {
+            InternalFields obj = Serializer.Parse<InternalFields>("{ \"InternalFieldWithAttribute\": \"123\" }");
+            Assert.That(obj.GetInternalFieldWithAttribute(), Is.EqualTo("123"));
+        }
+
+        [Test]
+        public void Internal_fields_are_emitted_according_to_attributes()
+        {
+            string json = Serializer.ToString(new InternalFields(), false);
+            Assert.That(json, Is.EqualTo("{\"InternalFieldWithAttribute\":\"333\"}"));
         }
     }
 }
