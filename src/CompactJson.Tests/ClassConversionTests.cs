d638732de4f822b7542aed964bd19497a9a3a9be
pupsette <weigole@gmx.de>
2019-06-03 19:42:02 +0200

    Added few more tests. Updated tests.

--- src/CompactJson.Tests/ClassConversionTests.cs
+++ src/CompactJson.Tests/ClassConversionTests.cs
@@ -1,5 +1,4 @@
 ﻿using NUnit.Framework;
-using NUnit.Framework.Constraints;
 using System;
 
 namespace CompactJson.Tests
@@ -30,45 +29,12 @@ namespace CompactJson.Tests
             public TestClass Nested { get; set; }
         }
 
-        private class TestClassPrivateMembers
+        private class TestClassIgnoredMember
         {
-            public TestClassPrivateMembers()
-            {
-            }
+            [JsonIgnoreMember]
+            public string Ignored { get; set; } = "Ignored";
 
-            public TestClassPrivateMembers(string privateSetterPropertyValue, string privatePropertyValue, string privateFieldValue, string privateReadonlyFieldValue)
-            {
-                PrivateSetter = privateSetterPropertyValue;
-                PrivateProperty = privatePropertyValue;
-                PrivateField = privateFieldValue;
-                PrivateReadOnlyField = privateReadonlyFieldValue;
-            }
-
-            internal readonly string writeOk;
-            public string WriteOk { get { return writeOk; } }
-
-            public string PrivateSetter { get; private set; }
-
-            private string PrivateProperty { get; set; }
-
-            private string PrivateField;
-
-            private readonly string PrivateReadOnlyField;
-        }
-
-        private class TestBaseClass
-        {
-            public int Base { get; set; }
-        }
-
-        private class TestClassDerivedA : TestBaseClass
-        {
-            public int ValueA { get; set; }
-        }
-
-        private class TestClassDerivedB : TestBaseClass
-        {
-            public int ValueB { get; set; }
+            public string Valid { get; set; } = "Valid";
         }
 
         [TestCase("{ \"Number\": 5 }", typeof(TestClass), "{\"Number\":5}")]
@@ -86,54 +52,12 @@ namespace CompactJson.Tests
         [TestCase("{ \"Nested\": {\"Number\":  190} }", typeof(TestClassNested), "{\"Nested\":{\"Number\":190}}")]
         [TestCase("{}", typeof(TestClassNested), "{}")]
         [TestCase("null", typeof(TestClassNested), "null")]
+        [TestCase("{}", typeof(TestClassIgnoredMember), "{\"Valid\":\"Valid\"}")]
         public void Conversion_to_model_and_back(string input, Type type, string expectedOutput)
         {
             object model = Serializer.Parse(input, type);
             string output = Serializer.ToString(model, false);
             Assert.That(output, Is.EqualTo(expectedOutput));
         }
-
-        //[TestCase("{ \"Int\": 16 }", typeof(int), "{\"Int\":16}")]
-        //[TestCase("{ \"String\": \"123\" }", typeof(string), "{\"String\":\"123\"}")]
-        //[TestCase("{ \"String\": null }", null, "{}")]
-        //[TestCase("{ \"Float\": 15 }", typeof(double), "{\"Float\":15}")]
-        //[TestCase("{ \"IntArray\": [1,2,3] }", typeof(int[]), "{\"IntArray\":[1,2,3]}")]
-        //[TestCase("{}", null, "{}")]
-        //public void Conversion_to_model_with_multi_property_of_type_object(string input, Type expectedPropertyType, string expectedOutput)
-        //{
-        //    TestClassMulti model = Serializer.Parse<TestClassMulti>(input);
-
-        //    Assert.That(model.Multi, expectedPropertyType != null ? (IResolveConstraint)Is.InstanceOf(expectedPropertyType) : Is.Null);
-        //    string output = Serializer.ToString(model, false);
-        //    Assert.That(output, Is.EqualTo(expectedOutput));
-        //}
-
-        //[TestCase("{}", null, "{}")]
-        //[TestCase("{\"A\":{}}", typeof(TestClassDerivedA), "{\"A\":{}}")]
-        //[TestCase("{\"A\":{\"ValueB\":100}}", typeof(TestClassDerivedA), "{\"A\":{}}")]
-        //[TestCase("{\"B\":{\"ValueB\":100}}", typeof(TestClassDerivedB), "{\"B\":{\"ValueB\":100}}")]
-        //public void Conversion_to_model_with_multi_property_of_derived_type(string input, Type expectedPropertyType, string expectedOutput)
-        //{
-        //    TestClassMulti model = Serializer.Parse<TestClassMulti>(input);
-
-        //    Assert.That(model.MultiDerived, expectedPropertyType != null ? (IResolveConstraint)Is.InstanceOf(expectedPropertyType) : Is.Null);
-        //    string output = Serializer.ToString(model, false);
-        //    Assert.That(output, Is.EqualTo(expectedOutput));
-        //}
-
-        //// invalid test class
-        //private class TestClassInvalidMultiType1
-        //{
-        //    [MultiTypeProperty("A", typeof(int))]
-        //    [MultiTypeProperty("A", typeof(double))]
-        //    public object Multi { get; set; }
-        //}
-
-        //[TestCase(typeof(TestClassInvalidMultiType1))]
-        //public void Invalid_MultiTypeProperty_should_throw(Type modelType)
-        //{
-        //    Assert.That(() => Serializer.Parse("{}", modelType), Throws.Exception);
-        //}
-
     }
 }
--- src/CompactJson.Tests/ClassMemberAccessabilityTests.cs
+++ src/CompactJson.Tests/ClassMemberAccessabilityTests.cs
@@ -1,43 +1,138 @@
 ﻿using NUnit.Framework;
-using NUnit.Framework.Constraints;
 using System;
+using System.Text;
 
 namespace CompactJson.Tests
 {
     [TestFixture]
     public class ClassMemberAccessabilityTests
     {
-        private class TestClass
+        #region NON PUBLIC PROPERTIES
+
+        private class NonPublicProperty
         {
-            public TestClass()
+            public string NonPublic1 { get; private set; } = "1";
+
+            internal string GetNonPublic2() { return NonPublic2; }
+            public string NonPublic2 { private get; set; } = "2";
+            internal string NonPublic3 { get; set; } = "3";
+            internal string GetNonPublic4() { return NonPublic4; }
+            protected string NonPublic4 { get; set; } = "4";
+            public string NonPublic5 { get; internal set; } = "5";
+            public string NonPublic6 { internal get; set; } = "6";
+            public string NonPublic7 { get; } = "7";
+        }
+
+        [Test]
+        public void Non_public_properties_are_ignored()
+        {
+            StringBuilder sb = new StringBuilder("{");
+            for (int i = 1; i <= 7; i++)
             {
+                if (i > 1)
+                    sb.Append(",");
+                sb.Append("\"NonPublic");
+                sb.Append(i);
+                sb.Append("\":\"123\"");
             }
+            sb.Append("}");
+            Console.WriteLine(sb);
+            NonPublicProperty obj = Serializer.Parse<NonPublicProperty>(sb.ToString());
+            Assert.That(obj.NonPublic1, Is.EqualTo("1"));
+            Assert.That(obj.GetNonPublic2(), Is.EqualTo("2"));
+            Assert.That(obj.NonPublic3, Is.EqualTo("3"));
+            Assert.That(obj.GetNonPublic4(), Is.EqualTo("4"));
+            Assert.That(obj.NonPublic5, Is.EqualTo("5"));
+            Assert.That(obj.NonPublic6, Is.EqualTo("6"));
 
-            public TestClass(string privateSetterPropertyValue, string privatePropertyValue, string privateReadonlyFieldValue)
-            {
-                PrivateSetter = privateSetterPropertyValue;
-                PrivateProperty = privatePropertyValue;
-                PrivateReadOnlyField = privateReadonlyFieldValue;
-            }
+            string json = Serializer.ToString(new NonPublicProperty(), false);
+            Assert.That(json, Is.EqualTo("{}"));
+        }
 
-            internal readonly string writeOk;
-            public string WriteOk { get { return writeOk; } }
-
-            public string PrivateSetter { get; private set; }
-
-            public string PrivateGetter { private get; set; }
-
-            private string PrivateProperty { get; set; }
+        private class NonPublicPropertyAttributed
+        {
+            [JsonProperty]
+            public string NonPublic1 { get; private set; } = "1";
 
             [JsonProperty]
-            public readonly string PrivateReadOnlyField;
+            public string NonPublic2 { private get; set; } = "2";
+
+            [JsonProperty]
+            internal string NonPublic3 { get; set; } = "3";
+
+            [JsonProperty]
+            protected string NonPublic4 { get; set; } = "4";
+
+            [JsonProperty]
+            public string NonPublic5 { get; internal set; } = "5";
+
+            [JsonProperty]
+            public string NonPublic6 { internal get; set; } = "6";
+
+            [JsonProperty]
+            public string NonPublic7 { get; } = "7";
+        }
+
+        [TestCase("NonPublic1")]
+        [TestCase("NonPublic2")]
+        [TestCase("NonPublic3")]
+        [TestCase("NonPublic4")]
+        [TestCase("NonPublic5")]
+        [TestCase("NonPublic6")]
+        public void Writing_and_reading_non_public_property(string propertyName)
+        {
+            string propertyPart = $"\"{propertyName}\":\"succeed\"";
+            string json = "{ " + propertyPart + " }";
+            NonPublicPropertyAttributed obj = Serializer.Parse<NonPublicPropertyAttributed>(json);
+
+            string serializedJson = Serializer.ToString(obj, false);
+
+            Assert.That(serializedJson.Contains(propertyPart), serializedJson);
+        }
+
+        [Test]
+        public void Writing_non_public_property_without_setter_throws()
+        {
+            Assert.That(() => Serializer.Parse<NonPublicPropertyAttributed>("{\"NonPublic7\":\"fail\"}"), Throws.Exception);
+        }
+
+        [Test]
+        public void Reading_non_public_property_without_setter_succeeds()
+        {
+            string json = Serializer.ToString(new NonPublicPropertyAttributed(), false);
+            Assert.That(json.Contains("\"NonPublic7\":\"7\""), json);
+        }
+
+        #endregion
+
+        #region PROPERTY WITHOUT GETTER
+
+        #endregion
+
+        #region READONLY_FIELDS
+
+        private class ReadonlyFieldClass
+        {
+            [JsonProperty]
+            public readonly string PrivateReadOnlyField = "90";
         }
 
         [Test]
         public void Parsing_into_readonly_field_should_fail()
         {
-            Assert.That(() => Serializer.Parse<TestClass>("{ \"PrivateReadOnlyField\": \"123\" }"), Throws.Exception);
+            Assert.That(() => Serializer.Parse<ReadonlyFieldClass>("{ \"PrivateReadOnlyField\": \"123\" }"), Throws.Exception);
         }
+
+        [Test]
+        public void Emitting_readonly_field_should_succeed()
+        {
+            string json = Serializer.ToString(new ReadonlyFieldClass(), false);
+            Assert.That(json, Is.EqualTo("{\"PrivateReadOnlyField\":\"90\"}"));
+        }
+
+        #endregion
+
+        #region PRIVATE FIELDS
 
         private class PrivateFields
         {
@@ -72,6 +167,10 @@ namespace CompactJson.Tests
             Assert.That(json, Is.EqualTo("{\"PrivateFieldWithAttribute\":\"333\"}"));
         }
 
+        #endregion
+
+        #region PROTECTED FIELDS
+
         private class ProtectedFields
         {
             [JsonProperty]
@@ -105,6 +204,10 @@ namespace CompactJson.Tests
             Assert.That(json, Is.EqualTo("{\"ProtectedFieldWithAttribute\":\"333\"}"));
         }
 
+        #endregion
+
+        #region INTERNAL FIELDS
+
         private class InternalFields
         {
             [JsonProperty]
@@ -137,5 +240,7 @@ namespace CompactJson.Tests
             string json = Serializer.ToString(new InternalFields(), false);
             Assert.That(json, Is.EqualTo("{\"InternalFieldWithAttribute\":\"333\"}"));
         }
+
+        #endregion
     }
 }
